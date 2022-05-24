2a61e1af19bd4450f8fe4be9e09659d4a859070c
Francois Spies <fcspies@gmail.com>
2022-05-24 15:31:06 -0400

    ts setup and study

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      Hello Fromm Typescript
+
+    </div>
+  );
+}
+
+export default App;
